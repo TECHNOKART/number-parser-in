de1c0c4d6c3d7606ac80828e02808cb086b3b06c
manastkcs <87489830+manastkcs@users.noreply.github.com>
2022-11-16 18:11:35 +0530

    lac lacs

--- number_parser/data/en-IN.py
+++ number_parser/data/en-IN.py
@@ -39,6 +39,8 @@ info = {
         "thousand": 1000,
         "lakh": 100000,
         "lakhs": 100000,
+        "lac":100000,
+        "lacs":100000,
         "crore": 10000000,
         "crores": 10000000,
         "trillion": 1000000000000,
